4b365dd50c7ec6c64872a1cfa0d6588b800f3ed4
Paul kearney <paulke@udel.edu>
2023-02-25 16:06:32 -0500

    finished 2

--- src/App.tsx
+++ src/App.tsx
@@ -7,49 +7,12 @@ import { StartAttempt } from "./components/StartAttempt";
 import { TwoDice } from "./components/TwoDice";
 import { CycleHoliday } from "./components/CycleHoliday";
 import { Counter } from "./components/Counter";
-
 function App(): JSX.Element {
     return (
         <div className="App">
             <header role="bannder" className="App-header">
                 UD CISC275 with React Hooks and TypeScript. Hello! Welcome
             </header>
-<<<<<<< HEAD
-            <Container>
-                <Row>
-                    <Col>
-                        <div className="App-col-header">Contact Info</div>
-                        <ul>
-                            <li>email: paulke@udel.edu</li>
-                            <li>
-                                Github:{" "}
-                                <a href="url">https://github.com/Paulke2</a>
-                            </li>
-                            <li>Phone:(302) 757-3407</li>
-                        </ul>
-                    </Col>
-                    <Col>
-                        <div className="App-col-header"> More about me</div>
-                        <img
-                            src={require("./myDogs.jpg")}
-                            width="375"
-                            height="500"
-                            alt="these are my dogs Maggie and Chloe"
-                        />
-                    </Col>
-                </Row>
-            </Container>
-
-            <h1>Paul Kearney</h1>
-            <header>Hello World</header>
-            <Button onClick={() => console.log("Hello World!")}>
-                Log Hello World
-            </Button>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
-=======
             <hr></hr>
             <Counter></Counter>
             <hr />
@@ -62,7 +25,6 @@ function App(): JSX.Element {
             <ChangeType></ChangeType>
             <hr />
             <CycleHoliday></CycleHoliday>
->>>>>>> upstream/task-state
         </div>
     );
 }
--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,29 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const multi = "mult";
+    const one = "free";
+    const [value, setValue] = useState<QuestionType>("short_answer_question");
+    return (
+        <div>
+            <Button
+                onClick={() =>
+                    setValue(
+                        value === "short_answer_question"
+                            ? "multiple_choice_question"
+                            : "short_answer_question"
+                    )
+                }
+            >
+                Change Type
+            </Button>
+            to
+            {value === "multiple_choice_question" ? (
+                <div>Multiple Choice</div>
+            ) : (
+                <div>Short Answer</div>
+            )}
+            .
+        </div>
+    );
 }
